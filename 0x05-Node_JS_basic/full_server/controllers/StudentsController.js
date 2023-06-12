3cdc2cb12c69cbe52a3998b9d9bd2e74726c0c46
Dawitdkt <dawitkinfu5@gmail.com>
2023-06-12 08:58:21 +0300

    small fix

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,27 +1,27 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static async getAllStudents(req, res) {
-    try {
-      const studentsByField = await readDatabase(req.databaseFilePath);
-      const fields = Object.keys(studentsByField).sort((a, b) =>
-        a.toLowerCase().localeCompare(b.toLowerCase())
-      );
-
-      res.status(200).send('This is the list of our students\n\n');
-
-      for (const field of fields) {
-        const students = studentsByField[field];
-        const count = students.length;
-        const list = students.join(', ');
-        res.write(`Number of students in ${field}: ${count}. List: ${list}\n`);
+    static async getAllStudents(req, res) {
+        try {
+          const studentsByField = await readDatabase(req.databaseFilePath);
+          const fields = Object.keys(studentsByField).sort((a, b) =>
+            a.toLowerCase().localeCompare(b.toLowerCase())
+          );
+    
+          let response = 'This is the list of our students\n\n';
+    
+          for (const field of fields) {
+            const students = studentsByField[field];
+            const count = students.length;
+            const list = students.join(', ');
+            response += `Number of students in ${field}: ${count}. List: ${list}\n`;
+          }
+    
+          res.status(200).send(response);
+        } catch (error) {
+          res.status(500).send(`Cannot load the database: ${error.message}`);
+        }
       }
-
-      res.end();
-    } catch (error) {
-      res.status(500).send(`Cannot load the database: ${error.message}`);
-    }
-  }
 
   static async getAllStudentsByMajor(req, res) {
     const { major } = req.params;
@@ -33,7 +33,11 @@ class StudentsController {
 
     try {
       const studentsByField = await readDatabase(req.databaseFilePath);
+      const fields = Object.keys(studentsByField).sort((a, b) =>
+            a.toLowerCase().localeCompare(b.toLowerCase())
+          );
       const students = studentsByField[major] || [];
+      console.log(studentsByField[major]);
       const list = students.join(', ');
 
       res.status(200).send(`List: ${list}`);
