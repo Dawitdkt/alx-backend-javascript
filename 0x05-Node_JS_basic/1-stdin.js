c7a43f7725b4292f148f4be666b8c5c4debff1ea
Dawitdkt <dawitkinfu5@gmail.com>
2023-06-16 19:32:56 +0300

    small fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-
 process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  
+
   if (name === 'exit') {
     console.log('\nThis important software is now closing');
     process.exit();
