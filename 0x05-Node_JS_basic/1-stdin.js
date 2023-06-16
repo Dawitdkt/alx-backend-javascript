91efc23475fef993e47e4d1a082cc444df8bc11f
Dawitdkt <dawitkinfu5@gmail.com>
2023-06-16 21:12:47 +0300

    small fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.once('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.end();
+  process.stdin.end();
 });
 
 process.stdin.once('end', () => {
