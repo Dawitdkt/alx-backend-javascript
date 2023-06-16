3e2ce1716a5023f8fc1f88acc8f077e5e244eac0
Dawitdkt <dawitkinfu5@gmail.com>
2023-06-16 21:43:13 +0300

    small fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.once('data', (data) => {
+process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdin.end();
+  process.stdout.write('This important software is now closing\n');
+  process.stdin.destroy();
 });
 
-process.stdin.once('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
