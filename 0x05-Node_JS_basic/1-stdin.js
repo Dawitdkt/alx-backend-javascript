cb260b4fff4c3dfb5c748f171e9bb5c705c3c201
Dawitdkt <dawitkinfu5@gmail.com>
2023-06-16 19:52:30 +0300

    ss

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,15 +6,15 @@ process.stdin.on('data', (input) => {
   const name = input.trim();
 
   if (name === 'exit') {
-    console.log('\nThis important software is now closing');
+    console.log('This important software is now closing');
     process.exit();
   } else {
     console.log(`Your name is: ${name}`);
-    console.log('\nWelcome to Holberton School, what is your name?');
+    console.log('Welcome to Holberton School, what is your name?');
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('\nThis important software is now closing');
+  console.log('This important software is now closing');
   process.exit();
 });
